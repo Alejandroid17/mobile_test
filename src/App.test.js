76de650c279ea2848e23606a71bd049c6676cfa3
Alejandro <alejandrofernandezm94@gmail.com>
2018-12-08 18:28:25 +0100

    test: add ItemList test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import {render} from 'react-testing-library'
+import ItemList from './components/ItemList'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+describe("ItemList component", () => {
+    test("Item list without values", () => {
+        let props = {
+            title: "Test title",
+            emptyMessage: "Test empty message",
+            list: [],
+        };
+
+        const {getByText} = render(<ItemList {...props}/>);
+        const titleNode = getByText(props.title);
+        const emptyMessageNode = getByText(props.emptyMessage);
+
+        expect(titleNode).toBeDefined();
+        expect(emptyMessageNode).toBeDefined();
+    });
+
+    test("Item list with values", () => {
+        let props = {
+            title: "Test title",
+            emptyMessage: "Test empty message",
+            list: ["Val1", "Val2", "Val3"],
+        };
+
+        const {getByText} = render(<ItemList {...props}/>);
+        const firstValue = getByText(props.list[1]);
+        const secondValue = getByText(props.list[2]);
+        const thirdValue = getByText(props.list[2]);
+
+        expect(firstValue).toBeDefined();
+        expect(secondValue).toBeDefined();
+        expect(thirdValue).toBeDefined();
+    })
 });
