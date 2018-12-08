13f236f40104853dad3daa19e41bbf7b243faa87
Alejandro <alejandrofernandezm94@gmail.com>
2018-12-08 20:37:22 +0100

    docs: comment ItemList component

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * ItemList component.
+ * Render a list of the values with a main title.
+ */
 export default class ItemList extends React.Component {
 
     render() {
+        /** List of values. If the list is empty, the empty message is shown. */
         const itemList = this.props.list <= 0 ?
             <div>{this.props.emptyMessage}</div> : this.props.list.map((item, index) =>
                 <li key={index} className="list-group-item text-truncate">{item}</li>);
