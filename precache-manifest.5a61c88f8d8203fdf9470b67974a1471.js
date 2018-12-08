4fe5d7f7d6ed4b54fccdf49578ff702fe8c99eac
root <alejandrofernandezm94@gmail.com>
2018-12-08 23:10:20 +0100

    Updates

---
+++ precache-manifest.5a61c88f8d8203fdf9470b67974a1471.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba19f3d810601426fc69",
+    "url": "/mobile_test/static/js/runtime~main.ba19f3d8.js"
+  },
+  {
+    "revision": "651a0b5dc55da2c985db",
+    "url": "/mobile_test/static/js/main.651a0b5d.chunk.js"
+  },
+  {
+    "revision": "0b86ff13969a1515e71d",
+    "url": "/mobile_test/static/js/1.0b86ff13.chunk.js"
+  },
+  {
+    "revision": "651a0b5dc55da2c985db",
+    "url": "/mobile_test/static/css/main.5b0d7644.chunk.css"
+  },
+  {
+    "revision": "0b86ff13969a1515e71d",
+    "url": "/mobile_test/static/css/1.d3578eb6.chunk.css"
+  },
+  {
+    "revision": "3a52ee34e07e3bf50f78ce282783d141",
+    "url": "/mobile_test/index.html"
+  }
+];
