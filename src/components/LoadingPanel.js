68657b0dbe1ad20865e1242fd8f95ed905fff065
Alejandro <alejandrofernandezm94@gmail.com>
2018-12-08 12:15:34 +0100

    task: add LoadingPanel component

---
+++ src/components/LoadingPanel.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {GridLoader} from 'react-spinners';
+import { css } from 'react-emotion';
+
+export default class LoadingPanel extends React.Component {
+
+    render() {
+        const override = css`display: inline-block;`;
+
+        return (
+            <div className="container-fluid" style={{height: "100vh", marginTop: "20%"}}>
+                <div className="row align-items-center">
+                    <div className="col-sm-12 col-md-12">
+                        <GridLoader sizeUnit={"px"}
+                                    size={50}
+                                    color={'#888C46'}
+                                    className={override}/>
+                    </div>
+                </div>
+                <div className="row align-items-center mt-5">
+                    <div className="col-sm-12 col-md-12">
+                        <h2 className="font-weight-bold">Loading</h2>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
